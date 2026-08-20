5a3c1cfc9b83d583bbc2cb034531b0842b69792b
zwhy-yjw <1596991129@qq.com>
2026-08-20 10:23:41 +0800

    算法：<DFS>

---
+++ DFS/n-皇后问题.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+const int N = 20;
+
+int n;
+char g[N][N];
+bool col[N], dg[N], udg[N];
+
+
+void dfs(int u)
+{
+    if(u == n)
+    {
+        for(int i = 0; i < n; i ++ ) puts(g[i]);
+        puts("");
+        return;
+    }
+    
+    for(int i = 0; i < n; i ++ )
+        if(!col[i] && !dg[u + i] && !udg[n - u + i])
+        {
+            g[u][i] = 'Q';
+            col[i] = dg[u + i] = udg[n - u + i] = true;
+            dfs(u + 1);
+            g[u][i] = '.';
+            col[i] = dg[u + i] = udg[n - u + i] = false;
+        }
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    cin >> n;
+    for(int i = 0; i < n; i ++ )
+        for(int j = 0; j < n; j ++ )
+            g[i][j] = '.';
+    
+    dfs(0);
+    
+    return 0;
+}
