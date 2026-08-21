4f777c30c7e88986d49d5c4ad671cdb2949ea332
zwhy-yjw <1596991129@qq.com>
2026-08-21 11:13:48 +0800

    添加分支文件，分类基础和提高

---
+++ 算法基础题/1.基础算法/1.快速排序/第k个数.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+const int N = 100010;
+int n, k;
+int q[N];
+int quick_sort(int l, int r, int k)
+{
+    if(l == r) return q[l];
+    int x = q[(l + r) >> 1], i = l - 1, j = r + 1;
+    while(i < j)
+    {
+        do i ++ ; while(q[i] < x);
+        do j -- ; while(q[j] > x);
+        if(i < j) swap(q[i], q[j]);
+    }
+    int sl = j - l + 1;
+    if(k <= sl) return quick_sort(l, j, k);
+    return quick_sort(j + 1, r, k - sl);
+}
+
+int main()
+{
+    cin >> n >> k;
+    for(int i = 0; i < n; i ++ ) cin >> q[i];
+
+    cout << quick_sort(0, n - 1, k) << endl;
+    return 0;
+}
+
