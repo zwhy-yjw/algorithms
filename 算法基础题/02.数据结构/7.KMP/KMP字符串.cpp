420b03f5f0ca2eaa47782eeb45a7a476f78b0960
zwhy-yjw <1596991129@qq.com>
2026-08-22 23:13:23 +0800

    算法基础题: KMP字符串 (数据结构-KMP)

---
+++ 算法基础题/02.数据结构/7.KMP/KMP字符串.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+const int N = 1e5 + 10, M = 1e6 + 10;
+char p[N], s[M];
+int n, m;
+int ne[N];
+
+int main()
+{
+    cin >> n >> p + 1 >> m >> s + 1;
+    //求ne过程
+    for(int i = 2, j = 0; i <= n; i ++ )
+    {
+        while(j && p[i] != p[j + 1]) j = ne[j];
+        if(p[i] == p[j + 1]) j ++ ;
+        ne[i] = j;
+    }
+    //kmp匹配过程
+    for(int i = 1, j = 0; i <= m; i ++ )
+    {
+        while(j && s[i] != p[j + 1]) j = ne[j];
+        if(s[i] == p[j + 1]) j ++ ;
+        if(j == n)
+        {
+            cout << i - n << ' ';
+            j = ne[j];
+        }
+    }
+    return 0;
+}
